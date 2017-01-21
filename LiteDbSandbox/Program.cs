7a3f60d58ddf2915631b7e69f809d35301f5f6ec
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2017-01-21 14:50:17 +0200

    Add project files.

---
+++ LiteDbSandbox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDbSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
